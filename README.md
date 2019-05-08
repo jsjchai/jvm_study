ac576b60cbc138f024c63ac9fc5600d8f3bcc117
jsjchai <jsjchai@163.com>
2019-05-08 23:27:30 +0800

    java堆溢出

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# jvm_study
-* OutofMemoryError
+## jvm_study
+### jvm args
+> \- 标准参数,所有的JVM都应该支持<br>
+  -X 非标准参数，每个JVM都应该实现<br>
+  -XX 不稳定参数（扩展参数），下一个版本可能会取消<br>
+  * -Xms 设置JVM初始堆内存
+  * -Xmx 设置JVM最大堆内存
+  * -XX:+HeapDumpOnOutOfMemoryError 内存溢出异常是dump出当前内存快照
--- src/com/github/jsjchai/oom/HeapOOM.java
+++ src/com/github/jsjchai/oom/HeapOOM.java
@@ -1,7 +1,24 @@
 package com.github.jsjchai.oom;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * VM args: -Xms30m -Xmx30m -XX:+HeapDumpOnOutOfMemoryError
+ * jdk version: 1.8
+ *
  * @author jsjchai.
  */
 public class HeapOOM {
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<>();
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+
+    private static class OOMObject {
+
+    }
 }
